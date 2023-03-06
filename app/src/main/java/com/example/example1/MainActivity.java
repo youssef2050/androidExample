c2b0d14f964341795cc08235ea88062177024edb
android course <yousf2050@hotmail.com>
2023-03-06 13:21:01 +0200

    int

---
+++ app/src/main/java/com/example/example1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.example1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void show(View v){
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
+    }
+}
